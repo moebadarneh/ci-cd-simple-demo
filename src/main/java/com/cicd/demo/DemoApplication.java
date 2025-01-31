1996fbd088a507e122aef87042503a62c41b7712
moebadarneh <115925072+moebadarneh@users.noreply.github.com>
2025-01-31 23:12:42 +0100

    Update DemoApplication.java

--- src/main/java/com/cicd/demo/DemoApplication.java
+++ src/main/java/com/cicd/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		 
-                System.out.println("Hello, Spring Boot! The application has started.");
+                SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
